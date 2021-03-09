11c635cb15fe6d99ab5acc237995f29f70eff477
Andre525 <ghost5run@yandex.ru>
2021-03-09 12:37:53 -0500

    Add directory mySimpleComputer

---
+++ mySimpleComputer/src/mySimpleComputer.c
@@ -0,0 +1,127 @@
+#include "mySimpleComputer.h"
+
+int sc_memoryInit()
+{
+    for (int i = 0; i < SIZE_OF_MEMORY; i++) {
+        RAM[i] = 0;
+    }
+    return 0;
+}
+
+int sc_memorySet(int adress, int value)
+{
+    if (adress < 0 || adress >= SIZE_OF_MEMORY) {
+        sc_regSet(M, 1);
+        return ERROR_EXIT_RANGE_ARRAY;
+    }
+    RAM[adress] = value;
+    return 0;
+}
+
+int sc_memoryGet(int adress, int* value)
+{
+    if (adress < 0 || adress >= SIZE_OF_MEMORY) {
+        sc_regSet(M, 1);
+        return ERROR_EXIT_RANGE_ARRAY;
+    }
+    *value = RAM[adress];
+    return 0;
+}
+
+int sc_memorySave(char* filename)
+{
+    FILE* file;
+    if ((file = fopen(filename, "wb")) == NULL) {
+        return ERROR_FWRITE; // ошибка записи в файл
+    }
+    fwrite(RAM, sizeof(int), SIZE_OF_MEMORY, file);
+    fclose(file);
+    return 0;
+}
+
+int sc_memoryLoad(char* filename)
+{
+    FILE* file;
+    if ((file = fopen(filename, "rb")) == NULL) {
+        return ERROR_FREAD; // ошибка чтения из файла
+    }
+    if (fread(RAM, sizeof(int), SIZE_OF_MEMORY, file) != SIZE_OF_MEMORY) {
+        if (feof(file)) {
+            return ERROR_FEOF;
+        } else {
+            return ERROR_FREAD;
+        }
+    }
+    return 0;
+}
+
+int sc_regInit()
+{
+    registr_of_flags = 0;
+    return 0;
+}
+
+int sc_regSet(int registr, int value)
+{
+    if (((value == 1) || (value == 0)) == false) { // проверка на допустимое значение
+        return ERROR_VALUE;
+    }
+    if ((registr == P || registr == O || registr == M || registr == T || registr == E) == false) { // проверка на допустимый номер регистра
+        return ERROR_RANK;
+    }
+    if (value == 1) {
+        registr_of_flags = registr_of_flags | registr;
+        // BIT_SET(registr_of_flags, registr);
+    } else if (value == 0) {
+        registr_of_flags = registr_of_flags & (~registr);
+        // BIT_DELETE(registr_of_flags, registr);
+    }
+    return 0;
+}
+
+int sc_regGet(int registr, int* value)
+{
+    if ((registr == P || registr == O || registr == M || registr == T || registr == E) == false) { // проверка на допустимый разряд
+        return ERROR_RANK;
+    }
+    if ((registr_of_flags & registr) != 0) {
+        *value = 1;
+    } else {
+        *value = 0;
+    }
+
+    return 0;
+}
+
+int sc_commandEncode(int command, int operand, int* value)
+{
+    if (command < 0x10 || (command > 0x11 && command < 0x20) || (command > 0x21 && command < 0x30) || (command > 0x33 && command < 0x40) || (command > 0x43 && command < 0x51) || command > 0x76) {
+        return ERROR_INVALID_COMMAND;
+    }
+    if (operand > 0x7F || operand < 0) {
+        return ERROR_INVALID_OPERAND;
+    }
+
+    *value = (command << 7) | operand;
+
+    return 0;
+}
+
+int sc_commandDecode(int value, int* command, int* operand)
+{
+    int attribute;
+    int temp_command, temp_operand;
+    attribute = value & 0x4000;
+    if (attribute != 0) {
+        return ERROR_NOT_COMMAND;
+    }
+    temp_operand = value & 0x7F;
+    temp_command = (value >> 7) & 0x7F;
+    if (temp_command < 0x10 || (temp_command > 0x11 && temp_command < 0x20) || (temp_command > 0x21 && temp_command < 0x30) || (temp_command > 0x33 && temp_command < 0x40) || (temp_command > 0x43 && temp_command < 0x51) || temp_command > 0x76) {
+        sc_regSet(E, 1);
+        return ERROR_INVALID_COMMAND;
+    }
+    *operand = temp_operand;
+    *command = temp_command;
+    return 0;
+}
---
+++ mySimpleComputer/src/mySimpleComputer.h
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#ifndef COMP_H
+#define COMP_H
+#define SIZE_OF_MEMORY 100
+
+/* МАСКИ ДЛЯ РЕГИСТРА ФЛАГОВ */
+#define P 0b00000001 // переполнение при выполнение операции
+#define O 0b00000010 // ошибка деления на 0
+#define M 0b00000100 // ошибка выхода за границы памяти
+#define T 0b00001000 // игнорирование тактовых импульсов
+#define E 0b00010000 // указана неверная команда
+/***************************************************************************/
+
+// #define BIT_CHECK(REG, RANK) ((REG >> RANK) & 0x1) // получить флаг
+// #define BIT_SET(REG, RANK) ((REG = ((REG) | (0x1 << RANK)))) // установить 1
+// #define BIT_DELETE(REG, RANK) (REG = ((REG) & (~(0x1 << RANK)))) // установить 0
+
+#define ERROR_EXIT_RANGE_ARRAY -1 // Ошибка выхода за границы массива
+#define ERROR_FWRITE -2 // Ошибка записи в файл (файл не открылся)
+#define ERROR_FREAD -3 // Ошибка чтения из файла
+#define ERROR_FEOF -4 // Ошибка: преждевременный конец файла
+#define ERROR_VALUE -5 // Ошибка: недопустимое значение
+#define ERROR_RANK -6 // Ошибка: недопустимый разряд регистра
+#define ERROR_INVALID_COMMAND -7 // Ошибка: неверная команда
+#define ERROR_INVALID_OPERAND -8 // Ошибка: неверный операнд
+#define ERROR_NOT_COMMAND -9 // Ошибка: это не команда
+
+int RAM[SIZE_OF_MEMORY];
+int registr_of_flags; // регистр флагов(переменная хранящая флаги)
+
+int sc_memoryInit();
+int sc_memorySet(int adress, int value);
+int sc_memoryGet(int adress, int* value);
+int sc_memorySave(char* filename);
+int sc_memoryLoad(char* filename);
+int sc_regInit();
+int sc_regSet(int registr, int value);
+int sc_regGet(int registr, int* value);
+int sc_commandEncode(int command, int operand, int* value);
+int sc_commandDecode(int value, int* command, int* operand);
+
+#endif
