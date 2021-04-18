aef5a061e92a7f8887f568b346f8d9ba735bfdc2
Andre525 <ghost5run@yandex.ru>
2021-04-18 21:54:42 +0700

    Delete unwanted code

--- src/mysignal.c
+++ src/mysignal.c
@@ -12,26 +12,6 @@ void my_timer(int signo)
         move(1);
     }
     Draw();
-    //     int value = 0;
-    //     sc_regGet(IGNR_TIME, &value);
-    //     if (instructionCounter < 99 && !value) {
-    //         instructionCounter++;
-    //     }
-    //     sc_regGet(IGNR_PULSES, &value);
-    //     if (!value) {
-    //         while (value == 0) {
-    //             // _print_screen_();
-    //             value = CU();
-    //             sleep(1);
-    //         }
-    //         if (value == 2) {
-    //             sc_regSet(IGNR_PULSES, 1);
-    //             alarm(0);
-    //         }
-    //     } else {
-    //         alarm(0);
-    //     }
-    //     // _print_screen_();
 }
 
 void my_signal(int signo)
